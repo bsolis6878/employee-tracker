5501d66cd29eafcee32edd95c1ffbe36a1944803
bsolis6878 <bsolis6878@gmail.com>
2022-09-30 13:22:48 -0500

    added database connection

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2')
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'uu7o2mz6',
+        database: 'organization'
+    }
+);
+
+module.exports = db;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const db = require('./db/connection');
 
 const optionsSelect = () => {
     return inquirer.prompt([
@@ -6,7 +7,7 @@ const optionsSelect = () => {
             type: 'list',
             name: 'continue',
             message: "Welcome to the employee tracker! Select an option below to continue.",
-            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
         }
     ])
 }
