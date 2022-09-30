31a72e821ef1907e8ec2eb7a3d4cfff5079e7be4
bsolis6878 <bsolis6878@gmail.com>
2022-09-30 13:57:27 -0500

    menu successfully loops and can quit

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const optionsSelect = () => {
     return inquirer.prompt([
         {
             type: 'list',
-            name: 'continue',
+            name: 'question',
             message: "Welcome to the employee tracker! Select an option below to continue.",
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Quit']
         }
     ])
-}
+    .then((answer) => {
+        if (answer.question === 'View all departments') {
+            console.log('Test');
+        }
+        if (answer.question === 'Quit') {
+            console.log('Thanks for dropping by!');
+            process.exit();
+        }
+    })
+    .then(optionsSelect);
+};
 
 optionsSelect();
