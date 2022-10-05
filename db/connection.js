f171709f975c0529c172b51036e1364281b1c928
bsolis6878 <bsolis6878@gmail.com>
2022-10-04 23:30:07 -0500

    installed and implemented dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'uu7o2mz6',
-        database: 'organization'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 );
 
